3c71978a76dd22b2bf085b3da9c4a7f80be1cff0
bahnirida <bahni.rida.contact@gmail.com>
2024-04-05 22:42:56 +0000

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { MyPortfolioComponent } from './components/my-portfolio/my-portfolio.component';
+import { AboutMeComponent } from './components/about-me/about-me.component';
+import { MyResumeComponent } from './components/my-resume/my-resume.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { MyServicesComponent } from './components/my-services/my-services.component';
+import { HeroSectionComponent } from './components/hero-section/hero-section.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MyPortfolioComponent,
+    AboutMeComponent,
+    MyResumeComponent,
+    FooterComponent,
+    HeaderComponent,
+    MyServicesComponent,
+    HeroSectionComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
