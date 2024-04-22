92c0ab9881d3a47bf6ca3c90edf642723047bcfe
bahnirida <bahni.rida.contact@gmail.com>
2024-04-22 23:21:19 +0100

    corrections2

--- src/app/components/my-portfolio/my-portfolio.component.ts
+++ src/app/components/my-portfolio/my-portfolio.component.ts
@@ -16,12 +16,12 @@ export class MyPortfolioComponent {
   ngOnInit(): void {
     // Initialize portfolioItems with your data
     this.portfolioItems = [
-      { title: 'eCommerce Website', imageUrl: '/assets/img/portfolio-1.jpg', filter: 1, delay: '0.0s',category:"WD" },
-      { title: 'Design Website', imageUrl: '/assets/img/portfolio-2.jpg', filter: 1, delay: '0.0s' ,category: "WD"},
-      { title: 'software Website', imageUrl: '/assets/img/portfolio-3.jpg', filter: 1, delay: '0.0s' ,category: "MA"},
-      { title: 'eCommerce Website', imageUrl: '/assets/img/portfolio-4.jpg', filter: 1, delay: '0.0s',category:"WD" },
-      { title: 'Design Website', imageUrl: '/assets/img/portfolio-5.jpg', filter: 1, delay: '0.0s' ,category: "MA"},
-      { title: 'software Website', imageUrl: '/assets/img/portfolio-6.jpg', filter: 1, delay: '0.0s',category: "WD" },
+      { title: 'eCommerce Website', imageUrl: './assets/img/portfolio-1.jpg', filter: 1, delay: '0.0s',category:"WD" },
+      { title: 'Design Website', imageUrl: './assets/img/portfolio-2.jpg', filter: 1, delay: '0.0s' ,category: "WD"},
+      { title: 'software Website', imageUrl: './assets/img/portfolio-3.jpg', filter: 1, delay: '0.0s' ,category: "MA"},
+      { title: 'eCommerce Website', imageUrl: './assets/img/portfolio-4.jpg', filter: 1, delay: '0.0s',category:"WD" },
+      { title: 'Design Website', imageUrl: './assets/img/portfolio-5.jpg', filter: 1, delay: '0.0s' ,category: "MA"},
+      { title: 'software Website', imageUrl: './assets/img/portfolio-6.jpg', filter: 1, delay: '0.0s',category: "WD" },
     ];
     this.filteredItems = [...this.portfolioItems]; // Initially show all items
   }
